2d551a22387ecb5d2d6cfb83513d3bee6638271b
Jorge Rodriguez E. <jirodrig@espol.edu.ec>
2015-01-14 17:08:21 -0500

    carga inicial

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jorge\documents\visual studio 2013\Projects\reinet\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
