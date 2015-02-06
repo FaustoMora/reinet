c609d3332f3a6e16af0fb553168f48c817e5ab80
Fausto Mora <patitodelt22@gmail.com>
2015-02-05 19:03:48 -0500

    views y templates de modulo Incubacion

--- concursoIncubacion/urls.py
+++ concursoIncubacion/urls.py
@@ -7,6 +7,9 @@ urlpatterns = patterns('',
 
 		url(r'^homeConcursos[/]?$',homeConcursos,name='homeConcursos'),
         url(r'^crearConcurso[/]?$',crearConcurso,name='crearConcurso'),
-        url(r'^verConcurso[/]?$',verConcurso,name='verConcurso')
+        url(r'^verConcurso[/]?$',verConcurso,name='verConcurso'),
+        url(r'^homeIncubacion[/]?$',homeIncubacion,name='homeIncubacion'),
+		url(r'^crearIncubacion[/]?$',crearIncubacion,name='crearIncubacion'),
+		url(r'^verIncubacion[/]?$',verIncubacion,name='verIncubacion')
 		
 )
--- concursoIncubacion/views.py
+++ concursoIncubacion/views.py
@@ -5,8 +5,6 @@ from forms import crearConcurso
 from models import Publicacion
 from models import DetalleConcurso
 
-def homeIncubaciones(request):
-    return render(request,'base.tpl.html')
 
 def homeConcursos(request):
 	return render_to_response('CONCURSO_inicio_concurso.html')
@@ -15,4 +13,13 @@ def crearConcurso(request):
 	return render_to_response('CONCURSO_crear_concurso.html')
 
 def verConcurso(request):
-	return render_to_response('CONCURSO_perfil.html')	
+	return render_to_response('CONCURSO_perfil.html')
+
+def homeIncubacion(request):
+	return render_to_response('INCUBACION_inicio.html')
+	
+def verIncubacion(request):
+	return render_to_response('INCUBACION_perfil.html')
+	
+def crearIncubacion(request):
+	return render_to_response('INCUBACION_crear.html')	
